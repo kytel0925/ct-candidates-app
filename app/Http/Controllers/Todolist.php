9d63bc0083dfa34f97e70d7385aa16e17d3c93dc
DevLop-EC <christlopbsc12@gmail.com>
2022-03-08 18:20:34 -0500

    CRUD todolist controller

--- app/Http/Controllers/Todolist.php
+++ app/Http/Controllers/Todolist.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Todolist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class TodoController extends Controller
 {
@@ -16,5 +17,62 @@ class TodoController extends Controller
             ->get();
 
         return response()->json($todos, 200);
+    }
+
+    public function markComplete(Todolist $todo)
+    {
+        if ($todo->is_completed == 0) {
+            $todo->is_completed = 1;
+        } else {
+            $todo->is_completed = 0;
+        }
+
+        $todo->save();
+
+        return response()->json($todo, 200);
+    }
+
+    public function reorder(Request $request)
+    {
+        $this->validate($request, [
+            'order' => 'required|array',
+        ]);
+
+        $order = $request->input('order');
+
+        DB::transaction(function () use ($order) {
+            foreach ($order as $value) {
+                Todolist::find($value['id'])->update(['order' => $value['order']]);
+            }
+        });
+
+        return response()->json([], 204);
+    }
+
+    public function store(Request $request)
+    {
+        $postData = $this->validate($request, [
+            'description' => ['required', 'min:3'],
+            'date' => ['sometimes', 'date'],
+        ]);
+
+        $todo = Todolist::create([
+            'description' => $postData['description'],
+            'ends_at' => $postData['date'],
+            'user_id' => Auth::user()->id,
+        ]);
+
+        return response()->json($todo, 201);
+    }
+
+    public function remove(Request $request)
+    {
+        $postData = $this->validate($request, [
+            'id' => ['required', 'exists:todos,id'],
+        ]);
+
+        Todolist::where('id', $postData['id'])->delete();
+
+        return response()->json("", 201);
     }
 }
